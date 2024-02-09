a3511c926ef5f60bf674c442954af334ab10b75f
Will Kurt <will@dottxt.co>
2024-02-09 14:43:03 -0500

    first commit, added basic files

---
+++ gsm8k_evals/db_tools.py
@@ -0,0 +1,83 @@
+import sqlite3
+from datetime import datetime
+
+def create_evaluation_table(db):
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        result = cur.execute("""
+        CREATE TABLE IF NOT EXISTS evaluations(model, dataset, 
+        start_time, end_time, sampler, prompt_name, struct_name);
+        """).fetchall()
+        con.commit()
+    return result
+
+def create_result_table(db):
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        result = cur.execute("""
+    CREATE TABLE IF NOT EXISTS results(eval_id, question_number, start_time, end_time, 
+    realized_prompt, raw_answer, answer, bad_parse, correct)
+    """).fetchall()
+        con.commit()
+    return result
+
+def add_evaluation(db, model, dataset, start_time, sampler, prompt_name, struct_name):
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        cur.execute("""
+        INSERT INTO evaluations (model, dataset, start_time, sampler, prompt_name, struct_name)
+        VALUES (?, ?, ?, ?, ?, ?)
+        """, [model, dataset, start_time, sampler, prompt_name, struct_name])
+        result = cur.lastrowid
+        con.commit()
+    return result
+
+def add_result(db, eval_id, question_number, start_time,
+               end_time, realized_prompt, raw_answer, answer, bad_parse, correct):
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        cur.execute("""
+        INSERT INTO results (eval_id, question_number, start_time, end_time,
+        realized_prompt, raw_answer, answer, bad_parse, correct) VALUES
+        (?, ?, ?, ?, ?, ?, ?, ?, ?)
+        """, [eval_id, question_number, start_time, 
+              end_time, realized_prompt, raw_answer,
+              answer, bad_parse, correct])
+        result = cur.lastrowid
+        con.commit()
+    return result
+        
+
+def update_evaluation_end(db, eval_id):
+    current_time = datetime.now()
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        result = cur.execute(
+            """
+            UPDATE evaluations SET end_time = ? WHERE rowid = ?
+            """, (current_time, eval_id)
+        ).fetchall()
+    return result
+
+def eval_results(db, eval_id):
+    with sqlite3.connect(db) as con:
+        cur = con.cursor()
+        result = cur.execute(
+            """
+            with summary as
+            (SELECT eval_id, count(eval_id) as total, avg(bad_parse) as pe, avg(correct) as acc
+            from results
+            where eval_id = ?
+            group by eval_id)
+            SELECT model, prompt_name, struct_name, total, pe, acc
+            from evaluations
+            join summary
+            on evaluations.rowid = summary.eval_id;
+            """, (eval_id,)
+        ).fetchone()
+    return result
+
+def display_eval_results(db, eval_id):
+    out = "{0}-{1}-{2} {3} obs - ACC: {4:0.4f} PE: {5:0.4f} time: <TBD>"
+    result = eval_results(db, eval_id)
+    return out.format(*result)
---
+++ gsm8k_evals/structure.py
@@ -0,0 +1,107 @@
+import json
+import re
+
+# Note: We don't allow leading zeros since 01 is not valid.
+# However we're technically not allowing the model to answer
+# '0' and none of the answers are 0. Tiny advantage we should keep in mind.
+regex_m_300 = r'\{"reasoning": "[\w \d\.\*\-=\+,\?/]{10,300}", "answer": [1-9][0-9]{0,9}\}'
+regex_m_400 = r'\{"reasoning": "[\w \d\.\*\-=\+,\?/]{10,400}", "answer": [1-9][0-9]{0,10}\}'
+regex_m_100_300 = r'\{"reasoning": "[\w \d\.\*\-=\+,\?/]{100,400}", "answer": [1-9][0-9]{0,10}\}'
+regex_m_100_400 = r'\{"reasoning": "[\w \d\.\*\-=\+,\?/]{100,400}", "answer": [1-9][0-9]{0,10}\}'
+regex_m_50_500 = r'\{"reasoning": "[\w \d\.\*\-=\+,\?/]{50,500}", "answer": [1-9][0-9]{0,10}\}'
+regex_hr_300 = r'\{\n    "reasoning": "[\w \d\.\*\-=\+,\?/]{10,300}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+regex_hr_100_400 = r'\{\n    "reasoning": "[\w \d\.\*\-=\+,\?/]{100,400}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+regex_hr_200_400 = r'\{\n    "reasoning": "[\w \d\.\*\-=\+,\?/]{200,400}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+regex_hr_100_500 = r'\{\n    "reasoning": "[\w \d\.\*\-=\+,\?/]{100,500}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+regex_hr_50_400 = r'\{\n    "reasoning": "[\w \d\.\*\-=\+,\?/]{50,400}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+regex_hr_50_500 = r'\{\n    "reasoning": "[\w \d\.\*\-=\+,\?/]{50,500}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+# very similar to the data set range
+regex_hr_50_1000 = r'\{\n    "reasoning": "[\w\s\d\.\*\-=+,\?/]{50,1000}",\n    "answer": [1-9][0-9]{0,9}\n   \}'
+regex_qa_300 = r'A: [\w \d\.\*\-=\+,\?/]{10,300}\. The answer is [1-9][0-9]{0,9}\.\n'
+regex_qa_50_500 = r'A: [\w \d\.\*\-=\+,\?/]{50,500}\. The answer is [1-9][0-9]{0,9}\.\n'
+
+def process_raw_json_response(response):
+    return json.loads(response)['answer']
+
+lm_eval_regex = r"The answer is ([1-9][0-9]{0,9})\."
+def process_eval_harness(resp):
+    # This is what the eval harness uses
+    results = re.findall(lm_eval_regex, resp)
+    if len(results) > 0:
+        val = re.sub(",","",results[0])
+        val = float(val)
+        return int(val)
+    else:
+        # a bit of hack, but none of the
+        # eval answers are 0.
+        return 0
+
+def process_unstruct_json(response):
+    try:
+        result = json.loads(response.strip('},')).get('answer',0)
+    except json.JSONDecodeError:
+        result = 0
+    except AttributeError:
+        result = 0
+    return result
+
+struct_info = {
+    'unstruct_qa': {
+        'regex': None,
+        'stop_at': ["Q:", "\n\n"],
+        'processor': process_eval_harness
+    },
+    'unstruct_json': {
+        'regex': None,
+        'stop_at': ["},"],
+        'processor': process_unstruct_json
+    },
+    'regex_m_300': {
+        'regex': regex_m_300,
+        'processor': process_raw_json_response
+    },
+    'regex_m_400': {
+        'regex': regex_m_400,
+        'processor': process_raw_json_response
+    },
+    'regex_m_50_500': {
+        'regex': regex_m_50_500,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_300': {
+        'regex': regex_hr_300,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_50_400':{
+        'regex': regex_hr_50_400,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_100_400':{
+        'regex': regex_hr_100_400,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_200_400':{
+        'regex': regex_hr_200_400,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_100_500':{
+        'regex': regex_hr_100_500,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_50_500':{
+        'regex': regex_hr_50_500,
+        'processor': process_raw_json_response
+    },
+    'regex_hr_50_1000':{
+        'regex': regex_hr_50_1000,
+        'processor': process_raw_json_response
+    },
+    'regex_qa_300': {
+        'regex': regex_qa_300,
+        'processor': process_eval_harness
+    },
+    'regex_qa_50_500': {
+        'regex': regex_qa_50_500,
+        'processor': process_eval_harness
+    }
+}
